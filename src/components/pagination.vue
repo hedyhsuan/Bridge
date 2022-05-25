fac91fa4391f4bf85e65d23fbc4f893d85187830
hhhsuan41 <hhhsuan41@gmail.com>
2022-05-26 01:52:09 +0800

    修正分頁按鈕效果

--- src/components/pagination.vue
+++ src/components/pagination.vue
@@ -2,6 +2,8 @@
     <div>
       <nav aria-label="Page navigation example">
         <ul class="pagination justify-content-center">
+          <!-- 左邊箭頭 -->
+          <!-- 如果沒有上一頁就不顯示箭頭 -->
           <li class="page-item " 
           :class="{'d-none':!page.has_pre}">
           <a class="page-link" href="#" @click="insideEvent(page.current_page-1)">
@@ -10,14 +12,15 @@
             </span>
           </a>
          </li>
-       <!-- 如果沒有下一頁就不顯示箭頭 -->
+       
        <!-- a標籤沒有disabled屬性，要加在li -->
          <li class="page-item" 
           v-for="item in page.total_pages" :key="item"
           :class="{'active':item === page.current_page}"> 
-          <a class="page-link" href="#" @click="insideEvent(item)">{{item}}</a>   
+          <a class="page-link pageNumber" href="#" @click="insideEvent(item)">{{item}}</a>   
         </li>
   <!-- 一定要有key做為當前頁面active的判別,當頁面的序號與當前頁面一樣時就會有填滿效果 -->
+
          <li class="page-item"
          :class="{'d-none':!page.has_next}">
          <a class="page-link" href="#" @click="insideEvent(page.current_page+1)">
@@ -26,6 +29,7 @@
           </span>
         </a>
       </li>
+
       </ul>
      </nav>
         
@@ -52,7 +56,7 @@ export default {
     methods: {
         insideEvent(item){
             this.$emit("switchPage",item);
-            // console.log(item);
+            console.log(item);
             // $emit(‘自定義事件’,要帶入的資料),透過此來觸發重新整理頁面的實體事件getProduct
 
         }
@@ -65,12 +69,20 @@ export default {
 color: black;
 background-color: transparent;
 }
+.pageNumber{
+  color: rgba(104, 95, 78, 0.283);
+}
+
 
 .page-item.active .page-link{
-    background-color: #faf9f6;
-    border-color: #faf9f6;
+    background-color: transparent;
+    border-color:transparent;
     color: black;
     
+}
+.page-link:focus{
+  box-shadow: none;
+
 }
 
     
